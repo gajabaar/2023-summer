182c19edaf94a69fa2684d56c47faf5f0ef630f5
Maker <sauravkhanal20@gmail.com>
2023-06-02 00:37:08 -0400

    Task1/Week1 Completed by SauravKhanal

---
+++ 1.gwitter/SauravKhanal/Populate/populate_comments.sql
@@ -0,0 +1,6 @@
+
+-- Insert data
+INSERT INTO COMMENTS (id, USER_ID, GWEET_ID, COMMENTS) VALUES (1, 22, 35, 'Proin eu mi. Nulla ac enim.');
+INSERT INTO COMMENTS (id, USER_ID, GWEET_ID, COMMENTS) VALUES (2, 60, 53, 'Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.');
+INSERT INTO COMMENTS (id, USER_ID, GWEET_ID, COMMENTS) VALUES (3, 15, 99, 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum.');
+
