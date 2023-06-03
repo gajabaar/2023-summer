d06c5f88c5eb7fcb548b3e1d4ab693b22e75ca2e
Susheel Thapa <susheel.thapa@outlook.com>
2023-06-03 13:44:32 +0545

    Bash script to install php in linux(ubuntu)

---
+++ 1.gwitter/susheel/install_php.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+apt install --no-install-recommends php8.1
+apt install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath php8.1-sqlite3
