544c26bea24c11e7e877259c5df250fca7825acd
Susheel Thapa <susheel.thapa@outlook.com>
2023-06-03 13:44:54 +0545

    Update profile.php to check if provide user exist in database or not

--- 1.gwitter/susheel/profile.php
+++ 1.gwitter/susheel/profile.php
@@ -13,8 +13,26 @@
     <?php
     $username = $_GET["username"];
     $password = $_GET["password"];
-    echo "Username=$username";
-    echo "Password=$password";
+    $userExist = false;
+
+
+    $database = new SQLite3('./gwitter.sqlite3');
+    $results = $database->query('SELECT NAME,PASSWORD FROM USERS');
+    while ($row = $results->fetchArray()) {
+        if ($row['NAME'] == $username) {
+            if ($row["PASSWORD"] == $password) {
+                $userExist = true;
+                break;
+            }
+        }
+    }
+
+    if ($userExist) {
+        echo "User $username successfully login";
+    } else {
+        echo "Username and password doesn't exist";
+    }
+
     ?>
 </body>
 
