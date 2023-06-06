3b6cb0557ae17740bdc410d86fc6d5de19461fb4
Susheel Thapa <susheel.thapa@outlook.com>
2023-06-06 19:10:10 +0545

    Seperated the html and logic part

--- 1.gwitter/susheel/php/login.php
+++ 1.gwitter/susheel/php/login.php
@@ -1,51 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home/ Gwitter</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="assets/css/style.css">
-
-</head>
 
 <body>
 
-    <div class="background flex-c-c">
-        <div class="container flex-c-c">
-            <form action="index.php" method="POST" class="flex-c-c">
-                <h1>Gwitter</h1>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Username</label>
-                    <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Gwitter username">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
+    <?php
+    include("utils/validate_user.php");
 
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
 
-        </div>
-    </div>
-    <!-- Vendor JS Files -->
-    <script src="assets/vendor/aos/aos.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
+    echo $_SERVER['REQUEST_METHOD'];
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+
+        $userExist = validateUser($username, $password);
+
+        echo $userExist;
+        if (true) {
+            session_start();
+            echo $username;
+
+            $_SESSION['username'] = $username;
+
+            header("Location: http://localhost:4000/php/profile.php");
+            die();
+        } else {
+            header("Location: http://localhost:4000/index.html");
+        }
+    } else {
+        header("Location: http://localhost:4000/index.html");
+        die();
+    }
+    ?>
 </body>
 
 </html>
