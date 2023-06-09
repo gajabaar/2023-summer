ff9a937f98dcf4326dd31e107a9bfc4a09c8b6be
Sulabh Ghimire <sulove_ghimirey@hotmail.com>
2023-06-09 18:06:48 +0545

    Task1/week2 (#75)
    * A database connection page establised
    
    * Index page added
    
    * Homepage created
    
    * Porfile page created
    
    * A login logic page added
    
    * A logout logic page created
    
    * A profile update page created
    
    * A page to show followers list created
    
    * A page to show all the following list created
    
    * The readme file in report folder section updated with week2 report.
    
    ---------
    
    Co-authored-by: Sulabh Ghimire <sulabhghimire@pop-os.localdomain>

---
+++ 1.gwitter/sulabhghimire/followers.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php 
+session_start();
+include "db_conn.php";
+
+if (!isset($_SESSION['user_id'])) {
+    // The user is already logged in, redirect them to the home page
+    header('Location: index.php');
+    exit;
+}
+
+$query= "SELECT UserName
+        FROM Users
+        WHERE UserID IN (
+            SELECT WhoID
+            FROM Connection
+            WHERE WhomID = :uid
+        )";
+$statement = $database->prepare($query);
+$statement->bindValue(':uid', $_SESSION['user_id']);
+$results = $statement->execute();
+
+?>
+<body>
+    <h2>Followers</h2>
+    <?php
+while ($row = $results->fetchArray()) {
+    echo $row['UserName'];
+    echo "<br>";
+}
+?>
+</body>
+</html>
