d438addbbca6f2090c2cee25631ccced9e78e7c6
sareeka61 <ghimiresarika61@gmail.com>
2023-06-16 17:47:20 +0545

    Modified the location of schema.sql

--- 1.gwitter/sarika/database/schema.sql
+++ 1.gwitter/sarika/database/schema.sql
@@ -1,7 +1,23 @@
 CREATE TABLE users(
     userID INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT, 
+    firstName VARCHAR(245) NOT NULL,
+    lastName VARCHAR(245) NOT NULL,
     username VARCHAR(245) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE followers(
+    userID INTEGER NOT NULL,
+    followersID INTEGER NOT NULL,
+    FOREIGN KEY(userID) REFERENCES users(userID),
+    FOREIGN KEY (followersID) REFERENCES users(userID)
+);
+
+CREATE TABLE followings(
+    userID INTEGER NOT NULL,
+    followingID INTEGER NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID),
+    FOREIGN KEY (followingID) REFERENCES users(userID)
 );
 
 CREATE TABLE gweets(
